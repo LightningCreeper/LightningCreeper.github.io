f977a07274fcf2e56aa11750a1bfb24f7cac5c96
LightningCreeper <l_ghtn_ngcr__p_r@outlook.com>
2024-07-27 16:04:38 +0800

    origin

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function changeheader() {
     var element = document.getElementsByTagName("header");
-    if (element.item[0].style == "height: 200px;") {
-        element.item[0].style = "height: 50px;";
+    if (element.item[0].style.height == '200px') {
+        element.item[0].style.height == '50px'
     } else {
-        element.item[0].style = "height: 200px;";
+        element.item[0].style.height == '200px'
     }
 }
