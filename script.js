131501803a927d1510792ac73729e90a0c2026bf
LightningCreeper <l_ghtn_ngcr__p_r@outlook.com>
2024-07-27 16:55:39 +0800

    origin

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function changeheader() {
     var element = document.getElementsByTagName("header");
     if (element.item[0].style.height == '200px') {
-        element.item[0].style.height == '50px'
+        element.item[0].style.height = '50px'
     } else {
-        element.item[0].style.height == '200px'
+        element.item[0].style.height = '200px'
     }
 }
 
