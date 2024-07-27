4145c5035898ab27b6c8724eae24e97276a96e8e
LightningCreeper <l_ghtn_ngcr__p_r@outlook.com>
2024-07-27 14:54:50 +0800

    origin

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeheader() {
+    var element = document.getElementsByTagName("header");
+    if (element.style == "height: 200px;") {
+        element.style == "height: 50px;";
+    } else {
+        element.style == "height: 200px;";
+    }
+}
