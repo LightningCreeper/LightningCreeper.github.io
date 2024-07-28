a4e4944afad0dc4daa668cdea54bcde67f1fb43a
LightningCreeper <l_ghtn_ngcr__p_r@outlook.com>
2024-07-28 09:37:19 +0800

    origin

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function changeheader(id, id2) {
     var element = document.getElementById(id);
     var element2 = document.getElementById(id2);
+    var element3 = document.getElementById(id3);
     if (element.style.height == '200px') {
         element.style.height = '50px';
         element2.style.padding = '50px 0px 0px 50px';
+        element3.style.display = 'none';
     } else {
         element.style.height = '200px';
         element2.style.padding = '200px 0px 0px 50px';
+        element3.style.display = 'block';
     }
 }
