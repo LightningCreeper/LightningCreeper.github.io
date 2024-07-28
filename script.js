942bd996d20dbfdc0f9e273fc263de43ccb1b867
LightningCreeper <l_ghtn_ngcr__p_r@outlook.com>
2024-07-28 09:22:17 +0800

    origin

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function changeheader(id, id2) {
-    var element = document.getElementsById(id);
-    var element2 = document.getElementsById(id2);
+    var element = document.getElementById(id);
+    var element2 = document.getElementById(id2);
     if (element.style.height == '200px') {
         element.style.height = '50px';
         element2.style.padding = '50px 0px 0px 50px';
