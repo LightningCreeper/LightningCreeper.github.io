d28aec52afa86e5aa6ec9f99d15e7230449b5d72
LightningCreeper <l_ghtn_ngcr__p_r@outlook.com>
2024-07-28 08:58:59 +0800

    origin

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ function changeheader(id, id2) {
         element2.style.padding = '50px 0px 0px 50px';
     } else {
         element.style.height = '200px';
-        element2.style.padding = '50px 0px 0px 50px';
+        element2.style.padding = '200px 0px 0px 50px';
     }
 }
